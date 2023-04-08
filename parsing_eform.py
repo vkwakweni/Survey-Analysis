713ca9ad0188139af63fd8b143c431749def6ec4
vkwakweni <tempvonline@gmail.com>
2023-04-08 09:35:16 -0400

    added some workflow files and planning parsing functions

--- parsing_eform.py
+++ parsing_eform.py
@@ -1,1 +1,15 @@
-# parses Google Forms
+import pandas as pd
+import numpy as np
+
+def read_questions_to_df():
+    pass
+
+def read_responses_to_df():
+    pass
+
+def read_questions_and_responses_to_df(file_name):
+    survey_data = pd.read_excel(io=file_name, sheet_name=None)  # will have to see if the dictionary returned uses sheet names as keys.
+    return survey_data
+    
+
+# will need something like an array to save responses
---
+++ workflows/tsom_employment_of_intl_students/tsom_student_employment_april_2023_workflow.py
@@ -0,0 +1,4 @@
+import parsing_eform
+
+testing = parsing_eform.read_questions_and_responses_to_df("Testing E-Forms (Questions).xlsx")
+debug = ":)"
