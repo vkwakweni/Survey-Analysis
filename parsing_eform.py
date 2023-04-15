4db0c1124c071716d4945ebca36857d7c8ce0038
vkwakweni <tempvonline@gmail.com>
2023-04-15 15:18:23 -0400

    handling pandas nan values

--- parsing_eform.py
+++ parsing_eform.py
@@ -2,13 +2,14 @@ import pandas as pd
 
 
 def read_questions_and_responses_totals(questions_file_name, responses_file_name):
-    questions = pd.read_excel(questions_file_name)
-    responses = pd.read_excel(responses_file_name)
+    questions = pd.read_csv(questions_file_name).dropna()
+    responses = pd.read_csv(responses_file_name).dropna()
+    # TODO: check what questions looks like when dropna'd
     survey_data = dict.fromkeys(questions.columns)
     for col in questions.columns:
         survey_data[col] = pd.DataFrame(data={}, index=questions[col].values, columns=[col])
         for value in questions[col].values:
-            survey_data.loc[[value, col]] = len(responses[responses[col] == value])
+            survey_data[col].at[value, col] = len(responses[responses[col] == value])
     return survey_data
 
 
