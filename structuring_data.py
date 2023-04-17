2d29521a7e10dfde6e00c8b2ce401fbd765fb4dc
vkwakweni <tempvonline@gmail.com>
2023-04-17 19:56:24 -0400

    tidied up the methods for the class

--- structuring_data.py
+++ structuring_data.py
@@ -3,28 +3,42 @@ import scipy.stats
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import scipy.stats as stats
-from scipy.stats import stats, ttest_ind
 
 
 # y = mx + c
 
 class LinReg:
-    def __init__(self, x, y):  # currently: x and y would be dataframe columns
+    def __init__(self, x, y):
         self.x = x  # this separation doesn't check length
         self.y = y
         self.model = LinearRegression().fit(self.x, self.y)
         self.r_sq = self.model.score(x, y)
         self.intercept = self.model.intercept_
         self.slope = self.model.coef_
-        self.rank_order_correlation = stats.spearmanr(x, y).correlation
-        self.p_value = stats.spearmanr(x, y).pvalue
+        x_listed = [v[0] for v in x.tolist()]
+        y_listed = y.tolist()
+        self.rank_order_correlation = stats.pearsonr(x_listed, y_listed)[0]
+        self.p_value = stats.pearsonr(x_listed, y_listed)[1]
+        self.x_shapiro_p = stats.shapiro(x)
+        self.y_shapiro_p = stats.shapiro(y)
+        self.y_pred = self.model.predict(x)
 
     def give_summary(self):
-        return {"coefficent of determiniation": self.r_sq,
+        return {"coefficient of determination": self.r_sq,
                 "intercept": self.intercept,
                 "slope": self.slope,
                 "rank-order correlation": self.rank_order_correlation,
-                "p-value": self.p_value}
+                "p-value": self.p_value,
+                "shapiro tests": {"x": self.x_shapiro_p[1], "y": self.y_shapiro_p[1]}}
+
+    def graph_data(self, show=False, save=False, output_file=""):
+        plt.plot(self.x, self.y, 'o')
+        y_pred = generate_predicted_line(self.x, self.y)
+        plt.plot(self.x, y_pred)
+        if show:
+            plt.show()
+        if save:
+            plt.savefig(output_file)
 
 
 def data_to_array(data, x_col_name, y_col_name):
@@ -33,34 +47,7 @@ def data_to_array(data, x_col_name, y_col_name):
     return x, y
 
 
-def generate_linear_regression_model(x, y):
-    model = LinearRegression().fit(x, y)
-
-
-def generate_metrics(x, y):
-    model = LinearRegression().fit(x, y)
-    r_sq = model.score(x, y)
-    intercept = model.intercept_  # = c
-    slope = model.coef_  # = m
-    spearman_nums = stats.spearmanr(x, y)
-    return {"coefficient of determination": r_sq,
-            "intercept": intercept,
-            "slope": slope,
-            "spearman rank-order correlation": spearman_nums.correlation,
-            "p-value": spearman_nums.pvalue}
-
-
 def generate_predicted_line(x, y):
     model = LinearRegression().fit(x, y)
     y_pred = model.predict(x)
     return y_pred
-
-
-def graph_data(x, y, show=False, save=False, output_file=""):
-    plt.plot(x, y, 'o')
-    y_pred = generate_predicted_line(x, y)
-    plt.plot(x, y_pred)
-    if show:
-        plt.show()
-    if save:
-        plt.savefig(output_file)
